c92227dfe1a3f751402845b44923d10602e17146
rafaelsoutoo <rafaelsouto70017@gmail.com>
2024-05-22 10:25:31 -0300

    structure project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
       <body style={{background: "#d0d9e2"}}>
         <Main />
